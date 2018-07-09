4a3c11dcaa2645f23ff817f448b3154114c42bbd
Jaganathan Palanisamy <jpalanis@redhat.com>
2018-07-09 23:11:06 +0530

    OSP 10 custom roles templates

---
+++ osp10_custom_roles/deploy.sh
@@ -0,0 +1,8 @@
+openstack overcloud deploy --templates \
+    --timeout 90 \
+    -r roles_data.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/ovs-dpdk-permissions.yaml \
+    -e network-environment.yaml
+
